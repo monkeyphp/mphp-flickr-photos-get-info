be6a402e7fdb551773c9d6a3df1080223db69279
David White <git@monkeyphp.com>
2013-03-20 13:30:30 +0000

    Added phpunit.xml and phpunit bootstrap.php file

---
+++ tests/bootstrap.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * bootstrap.php
+ *
+ * PHP Version  PHP 5.3.10
+ *
+ * @category   MphpFlickrPhotoGetInfoTest
+ * @author     David White [monkeyphp] <git@monkeyphp.com>
+ */
+require_once '../vendor/autoload.php';
