280034e236e115887bc17584650746f52b192f7f
David White <git@monkeyphp.com>
2013-03-21 11:35:14 +0000

    Updated ResultAdapter

--- src/MphpFlickrPhotoGetInfo/Adapter/Xml/Result/ResultAdapter.php
+++ src/MphpFlickrPhotoGetInfo/Adapter/Xml/Result/ResultAdapter.php
@@ -89,6 +89,234 @@ class ResultAdapter extends \MphpFlickrBase\Adapter\Xml\Result\AbstractResultAda
     }
 
     /**
+     * Return the secret property from the results
+     * 
+     * @return string|null
+     */
+    public function getSecret()
+    {
+        if (! isset($this->secret)) {
+            $this->secret = ($this->getDomElement()->getAttribute(self::ATTRIBUTE_SECRET));
+        }
+        return $this->secret;
+    }
+    
+    /**
+     * Return the server property from the results
+     * 
+     * @return string|null
+     */
+    public function getServer()
+    {
+        if (! isset($this->server)) {
+            $this->server = ($this->getDomElement()->getAttribute(self::ATTRIBUTE_SERVER));
+        }
+        return $this->server;
+    }
+    
+    /**
+     * Return the farm property from the results
+     * 
+     * @return string|null
+     */
+    public function getFarm()
+    {
+        if (! isset($this->farm)) {
+            $this->farm = ($this->getDomElement()->getAttribute(self::ATTRIBUTE_FARM));
+        }
+        return $this->farm;
+    }
+    
+    /**
+     * Return a UNIX timestamp/MySql date string representing the date uploaded 
+     * property from the results
+     * 
+     * @return string|null
+     */
+    public function getDateUploaded()
+    {
+        if (! isset($this->dateUploaded)) {
+            $this->dateUploaded = ($this->getDomElement()->getAttribute(self::ATTRIBUTE_DATE_UPLOADED));
+        }
+        return $this->dateUploaded;
+    }
+    
+    /**
+     * Return the isFavorite property from the results
+     * 
+     * The isfavorite attribute only makes sense for logged in users who 
+     * don't own the photo. 
+     * 
+     * @return string|null
+     */
+    public function getIsFavorite()
+    {
+        if (! isset($this->isFavorite)) {
+            $this->isFavorite = ($this->getDomElement()->getAttribute(self::ATTRIBUTE_IS_FAVORITE));
+        }
+        return $this->isFavorite;
+    }
+    
+    /**
+     * Return the license property from the results
+     * 
+     * @return string|null
+     */
+    public function getLicense()
+    {
+        if (! isset($this->license)) {
+            $this->license = ($this->getDomElement()->getAttribute(self::ATTRIBUTE_LICENSE));
+        }
+        return $this->license;
+    }
+    
+    /**
+     * Return the safety_level property from the results
+     * 
+     * @return string|null
+     */
+    public function getSafetyLevel()
+    {
+        if (! isset($this->safetyLevel)) {
+            $this->safetyLevel = ($this->getDomElement()->getAttribute(self::ATTRIBUTE_SAFETY_LEVEL));
+        }
+        return $this->safetyLevel;
+    }
+    
+    /**
+     * Return the rotation property from the results
+     * 
+     * The rotation attribute is the current clockwise rotation, 
+     * in degrees, by which the smaller image sizes differ from the original
+     * image.
+     * 
+     * @return string|null
+     */
+    public function getRotation()
+    {
+        if (! isset($this->rotation)) {
+            $this->rotation = ($this->getDomElement()->getAttribute(self::ATTRIBUTE_ROTATION));
+        }
+        return $this->rotation;
+    }
+
+    /**
+     * Return the original_secret property from the results
+     * 
+     * @return string|null
+     */
+    public function getOriginalSecret()
+    {
+        if (! isset($this->originalSecret)) {
+            $this->originalSecret = ($this->getDomElement()->getAttribute(self::ATTRIBUTE_ORIGINAL_SECRET));
+        }
+        return $this->originalSecret;
+    }
+    
+    /**
+     * Return the original format property from the results
+     * 
+     * @return string|null
+     */
+    public function getOriginalFormat()
+    {
+        if (! isset($this->originalFormat)) {
+            $this->originalFormat = ($this->getDomElement()->getAttribute(self::ATTRIBUTE_ORIGINAL_FORMAT));
+        }
+        return $this->originalFormat;
+    }
+    
+    /**
+     * Return the owner nsid value from the results
+     * 
+     * @return string|null
+     */
+    public function getOwnerNsid()
+    {
+        if (! isset($this->ownerNsid)) {
+            $this->ownerNsid = $this->getDomXPath($this->getDomDocument())->query(self::QUERY_OWNER_NSID)->item(0)->value;
+        }
+        return $this->ownerNsid;
+    }
+    
+    /**
+     * Return the owner username value from the results
+     * 
+     * @return string|null
+     */
+    public function getOwnerUsername()
+    {
+        if (! isset($this->ownerUsername)) {
+            $this->ownerUsername = $this->getDomXPath($this->getDomDocument())->query(self::QUERY_OWNER_USERNAME)->item(0)->value;
+        }
+        return $this->ownerUsername;
+    }
+    
+    /**
+     * Return the owner realname property from the results
+     * 
+     * @return string|null
+     */
+    public function getOwnerRealname()
+    {
+        if (! isset($this->ownerRealname)) {
+            $this->ownerRealname = $this->getDomXPath($this->getDomDocument())->query(self::QUERY_OWNER_REALNAME)->item(0)->value;
+        }
+        return $this->ownerRealname;
+    }
+    
+    /**
+     * Return the owner location property from the results
+     * 
+     * @return string|null
+     */
+    public function getOwnerLocation()
+    {
+        if (! isset($this->ownerRealLocation)) {
+            $this->ownerLocation = $this->getDomXPath($this->getDomDocument())->query(self::QUERY_OWNER_LOCATION)->item(0)->value;
+        }
+        return $this->ownerLocation;
+    }
+    
+    public function getOwnerIconServer()
+    {
+
+    }
+    
+    /**
+     * Return the iconfarm value from the results
+     * 
+     * @return string|null
+     */
+    public function getOwnerIconFarm()
+    {
+        if (! isset($this->ownerIconFarm)) {
+            $this->ownerIconFarm = $this->getDomXPath($this->getDomDocument())->query(self::QUERY_OWNER_ICONFARM)->item(0)->value;
+        }
+        return $this->ownerIconFarm;
+    }
+    
+    /**
+     * Return the owner path_alias property from the results
+     * 
+     * @return string|null
+     */
+    public function getOwnerPathAlias()
+    {
+
+    }
+
+    /**
+     * Return the title property from the results
+     * 
+     * @return string|null
+     */
+    public function getTitle()
+    {
+
+    }
+
+    /**
      * Return the description property
      *
      * @return string|null
@@ -104,6 +332,40 @@ class ResultAdapter extends \MphpFlickrBase\Adapter\Xml\Result\AbstractResultAda
     }
 
     /**
+     * 
+     */
+    public function getVisibilityIsPublic()
+    {
+
+    }
+
+    public function getVisibilityIsFriend()
+    {
+
+    }
+    
+    public function getVisibilityIsFamily()
+    {
+
+    }
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    /**
      * Return the comments property
      *
      * @return string|null
@@ -118,13 +380,7 @@ class ResultAdapter extends \MphpFlickrBase\Adapter\Xml\Result\AbstractResultAda
         return $this->comments;
     }
 
-    public function getDateUploaded()
-    {
-        if (! isset($this->dateUploaded)) {
-            $this->dateUploaded = ($this->getDomElement()->getAttribute(self::ATTRIBUTE_DATE_UPLOADED));
-        }
-        return $this->dateUploaded;
-    }
+    
 
     public function getDatesPosted()
     {
@@ -171,23 +427,11 @@ class ResultAdapter extends \MphpFlickrBase\Adapter\Xml\Result\AbstractResultAda
 
     }
 
-    public function getFarm()
-    {
-        if (! isset($this->farm)) {
-            $this->farm = ($this->getDomElement()->getAttribute(self::ATTRIBUTE_FARM));
-        }
-        return $this->farm;
-    }
+    
 
 
 
-    public function getIsFavorite()
-    {
-        if (! isset($this->isFavorite)) {
-            $this->isFavorite = ($this->getDomElement()->getAttribute(self::ATTRIBUTE_IS_FAVORITE));
-        }
-        return $this->isFavorite;
-    }
+    
 
     public function getMedia()
     {
@@ -202,56 +446,9 @@ class ResultAdapter extends \MphpFlickrBase\Adapter\Xml\Result\AbstractResultAda
 
     }
 
-    public function getOriginalFormat()
-    {
-        if (! isset($this->originalFormat)) {
-            $this->originalFormat = ($this->getDomElement()->getAttribute(self::ATTRIBUTE_ORIGINAL_FORMAT));
-        }
-        return $this->originalFormat;
-    }
 
-    public function getOriginalSecret()
-    {
-        if (! isset($this->originalSecret)) {
-            $this->originalSecret = ($this->getDomElement()->getAttribute(self::ATTRIBUTE_ORIGINAL_SECRET));
-        }
-        return $this->originalSecret;
-    }
 
-    public function getOwnerIconFarm()
-    {
-
-    }
-
-    public function getOwnerIconServer()
-    {
-
-    }
-
-    public function getOwnerLocation()
-    {
-
-    }
-
-    public function getOwnerNsid()
-    {
-
-    }
-
-    public function getOwnerPathAlias()
-    {
-
-    }
-
-    public function getOwnerRealname()
-    {
-
-    }
-
-    public function getOwnerUsername()
-    {
-
-    }
+    
 
     public function getPeopleHasPeople()
     {
@@ -268,47 +465,19 @@ class ResultAdapter extends \MphpFlickrBase\Adapter\Xml\Result\AbstractResultAda
 
     }
 
-    public function getRotation()
-    {
-        if (! isset($this->rotation)) {
-            $this->rotation = ($this->getDomElement()->getAttribute(self::ATTRIBUTE_ROTATION));
-        }
-        return $this->rotation;
-    }
+    
+    
 
-    public function getSafetyLevel()
-    {
-        if (! isset($this->safetyLevel)) {
-            $this->safetyLevel = ($this->getDomElement()->getAttribute(self::ATTRIBUTE_SAFETY_LEVEL));
-        }
-        return $this->safetyLevel;
-    }
+    
 
-    public function getSecret()
-    {
-        if (! isset($this->secret)) {
-            $this->secret = ($this->getDomElement()->getAttribute(self::ATTRIBUTE_SECRET));
-        }
-        return $this->secret;
-    }
-
-    public function getServer()
-    {
-        if (! isset($this->server)) {
-            $this->server = ($this->getDomElement()->getAttribute(self::ATTRIBUTE_SERVER));
-        }
-        return $this->server;
-    }
+    
 
     public function getTags()
     {
 
     }
 
-    public function getTitle()
-    {
-
-    }
+    
 
     public function getUrls()
     {
@@ -343,26 +512,9 @@ class ResultAdapter extends \MphpFlickrBase\Adapter\Xml\Result\AbstractResultAda
         return $this->views;
     }
 
-    public function getVisibilityIsFamily()
-    {
+    
 
-    }
 
-    public function getVisibilityIsFriend()
-    {
-
-    }
-
-    public function getVisibilityIsPublic()
-    {
-
-    }
-
-    public function getLicense()
-    {
-        if (! isset($this->license)) {
-            $this->license = ($this->getDomElement()->getAttribute(self::ATTRIBUTE_LICENSE));
-        }
-        return $this->license;
-    }
+    
+    
 }
