42ec69ec50f39c5e37917332f9b3462c37bf25e7
David White <git@monkeyphp.com>
2013-03-30 23:37:25 +0000

    Added TagResultAdapter.php

---
+++ src/MphpFlickrPhotosGetInfo/Adapter/Xml/Result/TagResultAdapter.php
@@ -0,0 +1,50 @@
+<?php
+/**
+ * TagResultAdapter.php
+ *
+ * PHP Version  PHP 5.3.10
+ *
+ * @category   MphpFlickrPhotosGetInfo
+ * @package    MphpFlickrPhotosGetInfo
+ * @subpackage MphpFlickrPhotosGetInfo\Adapter\Xml\Result
+ * @author     David White [monkeyphp] <git@monkeyphp.com>
+ */
+namespace MphpFlickrPhotosGetInfo\Adapter\Xml\Result;
+
+/**
+ * TagResultAdapter
+ *
+ * @category   MphpFlickrPhotosGetInfo
+ * @package    MphpFlickrPhotosGetInfo
+ * @subpackage MphpFlickrPhotosGetInfo\Adapter\Xml\Result
+ * @author     David White [monkeyphp] <git@monkeyphp.com>
+ */
+class TagResultAdapter extends \MphpFlickrBase\Adapter\Xml\Result\AbstractResultAdapter implements \MphpFlickrPhotosGetInfo\Adapter\Interfaces\Result\TagResultAdapterInterface
+{
+
+    public function getAuthor()
+    {
+
+    }
+
+    public function getId()
+    {
+
+    }
+
+    public function getMachineTag()
+    {
+
+    }
+
+    public function getRaw()
+    {
+
+    }
+
+    public function getTag()
+    {
+
+    }
+    
+}
