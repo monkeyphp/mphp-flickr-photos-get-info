3584c2d31ab0dcce625b6c106026c024f84496c4
David White <git@monkeyphp.com>
2013-03-31 10:49:24 +0100

    Implemented all interface methods in TagResultAdapter.php

--- src/MphpFlickrPhotosGetInfo/Adapter/Xml/Result/TagResultAdapter.php
+++ src/MphpFlickrPhotosGetInfo/Adapter/Xml/Result/TagResultAdapter.php
@@ -22,29 +22,99 @@ namespace MphpFlickrPhotosGetInfo\Adapter\Xml\Result;
 class TagResultAdapter extends \MphpFlickrBase\Adapter\Xml\Result\AbstractResultAdapter implements \MphpFlickrPhotosGetInfo\Adapter\Interfaces\Result\TagResultAdapterInterface
 {
 
+    protected $author;
+
+    protected $authorQuery;
+
+    protected $id;
+
+    protected $idQuery;
+
+    protected $machineTag;
+
+    protected $machineTagQuery;
+
+    protected $raw;
+
+    protected $rawQuery;
+
+    protected $tag;
+    
+    protected $taguery;
+
     public function getAuthor()
     {
+        if (! isset($this->author)) {
+            $this->author = (($nodeList = $this->getDomXPath($this->getDomDocument())->query($this->getAuthorQuery())) && $nodeList->length)
+                ? $nodeList->item(0)->value
+                : null;
+        }
+        return $this->author;
+    }
 
+    protected function getAuthorQuery()
+    {
+        return $this->authorQuery;
     }
 
     public function getId()
     {
+        if (! isset($this->id)) {
+            $this->id = (($nodeList = $this->getDomXPath($this->getDomDocument())->query($this->getIdQuery())) && $nodeList->length)
+                ? $nodeList->item(0)->value
+                : null;
+        }
+        return $this->id;
+    }
 
+    protected function getIdQuery()
+    {
+        return $this->idQuery;
     }
 
     public function getMachineTag()
     {
+        if (! isset($this->machineTag)) {
+            $this->machineTag = (($nodeList = $this->getDomXPath($this->getDomDocument())->query($this->getMachineTagQuery())) && $nodeList->length)
+                ? $nodeList->item(0)->value
+                : null;
+        }
+        return $this->machineTag;
+    }
 
+    protected function getMachineTagQuery()
+    {
+        return $this->machineTagQuery;
     }
 
     public function getRaw()
     {
+        if (! isset($this->raw)) {
+            $this->raw = (($nodeList = $this->getDomXPath($this->getDomDocument())->query($this->getRawQuery())) && $nodeList->length)
+                ? $nodeList->item(0)->value
+                : null;
+        }
+        return $this->raw;
+    }
 
+    protected function getRawQuery()
+    {
+        return $this->rawQuery;
     }
 
     public function getTag()
     {
+        if (! isset($this->tag)) {
+            $this->author = (($nodeList = $this->getDomXPath($this->getDomDocument())->query($this->getTagQuery())) && $nodeList->length)
+                ? $nodeList->item(0)->nodeValue
+                : null;
+        }
+        return $this->tag;
+    }
 
+    protected function getTagQuery()
+    {
+        return $this->tagQuery;
     }
 
 }
