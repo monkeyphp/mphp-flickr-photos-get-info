f99fbea93e3f5fefc93733337e11b5a68511647a
David White <git@monkeyphp.com>
2013-03-31 21:50:26 +0100

    Implemented methods in UrlResult.php

--- src/MphpFlickrPhotosGetInfo/Result/UrlResult.php
+++ src/MphpFlickrPhotosGetInfo/Result/UrlResult.php
@@ -22,4 +22,40 @@ namespace MphpFlickrPhotosGetInfo\Result;
 class UrlResult extends \MphpFlickrBase\Result\AbstractResult
 {
 
+    /**
+     * Set the adapter instance
+     *
+     * @param \MphpFlickrPhotosGetInfo\Adapter\Interfaces\Result\ResultAdapterInterface $adapter
+     *
+     * @throws \MphpFlickrBase\Excpetion\InvalidAdapterException
+     * @return \MphpFlickrPhotosGetInfo\Result\NoteResult
+     */
+    public function setAdapter(\MphpFlickrBase\Adapter\Interfaces\Result\ResultAdapterInterface $adapter)
+    {
+        if ($adapter instanceof \MphpFlickrPhotosGetInfo\Adapter\Interfaces\Result\UrlResultAdapterInterface) {
+            return parent::setAdapter($adapter);
+        }
+        throw new \MphpFlickrBase\Exception\InvalidAdapterException('Invalid adapter supplied');
+    }
+
+    /**
+     * Return the type value
+     * 
+     * @return string
+     */
+    public function getType()
+    {
+        return $this->getAdapter()->getType();
+    }
+
+    /**
+     * Return the textual url value
+     *
+     * @return string|null
+     */
+    public function getUrl()
+    {
+        return $this->getAdapter()->getUrl();
+    }
+
 }
