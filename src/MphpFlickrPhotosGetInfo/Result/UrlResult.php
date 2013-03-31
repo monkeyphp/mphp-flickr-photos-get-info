ee3288f50da899410255e1b2205caa60c4d1a171
David White <git@monkeyphp.com>
2013-03-31 21:46:54 +0100

    Added UrlResult.php

---
+++ src/MphpFlickrPhotosGetInfo/Result/UrlResult.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * UrlResult.php
+ *
+ * PHP Version  PHP 5.3.10
+ *
+ * @category   MphpFlickrPhotosGetInfo
+ * @package    MphpFlickrPhotosGetInfo
+ * @subpackage MphpFlickrPhotosGetInfo\Result
+ * @author     David White [monkeyphp] <git@monkeyphp.com>
+ */
+namespace MphpFlickrPhotosGetInfo\Result;
+
+/**
+ * UrlResult
+ *
+ * @category   MphpFlickrPhotosGetInfo
+ * @package    MphpFlickrPhotosGetInfo
+ * @subpackage MphpFlickrPhotosGetInfo\Result
+ * @author     David White [monkeyphp] <git@monkeyphp.com>
+ */
+class UrlResult extends \MphpFlickrBase\Result\AbstractResult
+{
+
+}
