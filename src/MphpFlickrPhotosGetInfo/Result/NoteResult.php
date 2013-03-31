13123a84c9f96906ba7dfba207f470752dfc7a18
David White <git@monkeyphp.com>
2013-03-31 19:29:36 +0100

    Added type casting to methods

--- src/MphpFlickrPhotosGetInfo/Result/NoteResult.php
+++ src/MphpFlickrPhotosGetInfo/Result/NoteResult.php
@@ -38,44 +38,92 @@ class NoteResult extends \MphpFlickrBase\Result\AbstractResult
         throw new \MphpFlickrBase\Exception\InvalidAdapterException('Invalid adapter supplied');
     }
 
+    /**
+     * Return the author name
+     *
+     * @return string|null
+     */
     public function getAuthorName()
     {
         return $this->getAdapter()->getAuthorName();
     }
 
+    /**
+     * Return the author nsid
+     *
+     * @return string|null
+     */
     public function getAuthorNsid()
     {
         return $this->getAdapter()->getAuthorNsid();
     }
 
+    /**
+     * Return the h value
+     *
+     * @return integer|null
+     */
     public function getH()
     {
-        return $this->getAdapter()->getH();
+        return (null !== ($h = $this->getAdapter()->getH()))
+            ? (integer)$h
+            : null;
     }
 
+    /**
+     * Return the id
+     *
+     * @return string|null
+     */
     public function getId()
     {
         return $this->getAdapter()->getId();
     }
 
+    /**
+     * Return the textual value of the Note
+     *
+     * @return string|null
+     */
     public function getNote()
     {
         return $this->getAdapter()->getNote();
     }
 
+    /**
+     * Return the w value
+     *
+     * @return integer|null
+     */
     public function getW()
     {
-        return $this->getAdapter()->getW();
+        return (null !== ($w = $this->getAdapter()->getW()))
+            ? (integer)$w
+            : null;
     }
 
+    /**
+     * Return the x value
+     *
+     * @return integer|null
+     */
     public function getX()
     {
-        return $this->getAdapter()->getX();
+         return (null !== ($x = $this->getAdapter()->getX()))
+            ? (integer)$x
+            : null;
     }
 
+    /**
+     * Return the y value
+     *
+     * @return integer|null
+     */
     public function getY()
     {
-        return $this->getAdapter()->getY();
+         return (null !== ($y = $this->getAdapter()->getY()))
+            ? (integer)$y
+            : null;
     }
-    
+
 }
